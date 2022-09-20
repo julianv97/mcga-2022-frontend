f3d7dcbdc973611e01ee4e3596446bcb01ed7a6b
julianv97 <julian.vicente@radiumrocket.com>
2022-09-20 13:49:23 -0300

    add navigation between screens

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Button';
 import styles from './login.module.css';
 
 const Login = () => {
+  const navigate = useNavigate()
   return (
     <div className={styles.container}>
       <div>Login</div>
-      <Button text={'Ingresar'} onClick={() => console.log('Ingresar')} />
+      <Button text={'Ingresar'} onClick={() => navigate("/products")} />
     </div>
   );
 };
--- src/screens/Products/index.js
+++ src/screens/Products/index.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from '../../components/Button';
+import styles from './products.module.css';
 
 const Products = () => {
+  const navigate = useNavigate()
   return (
-    <div>Products</div>
-  )
-}
+    <div className={styles.container}>
+      <div>Productos</div>
+      <Button text={'Cerrar sesión'} onClick={() => navigate("/login")} />
+      <a href="/login">Esto recarga la página</a>
+    </div>
+  );
+};
 
-export default Products
+export default Products;
+
+
+
+/* The href attribute would trigger a page refresh which would reset the application states. However the link and navlink of react-router doesn't trigger a page refresh. Since React is used to create single page applications most of the time make sure you choose Link or Navlink when working with routing */
