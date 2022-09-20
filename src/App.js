0169e2b7e02be3041fa9f91c7df2b728b81e852e
julianv97 <julian.vicente@radiumrocket.com>
2022-09-20 13:59:08 -0300

    create basic layout

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from './screens/Login';
 import Products from './screens/Products';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/products" element={<Products />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </Layout>
   );
 }
 
