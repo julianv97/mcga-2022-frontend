1d0238e72382f6725bcfee0950f67a42d84c66b4
julianv97 <julian.vicente@radiumrocket.com>
2022-11-03 09:27:24 -0300

    implement form library

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form';
+
 //import styles from './login.module.css'
 
 const Login = () => {
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm();
+
+    const onSubmit = (data) => {
+      console.log(data);
+    };
+
+    console.log(errors);
+
   return (
-      <div>
-        <h2>Login</h2>
-        <Link to={'/'}>Ir al Welcome</Link>
-      </div>
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Email</label>
+        <input type={'email'} {...register('email', { required: true})} />
+        {errors.email && <span>This field is required</span>}
+
+        <label>Password</label>
+        <input type={'password'} {...register('password', { required: true, minLenght: 6 })} />
+
+
+        <button type="submit">Login</button>
+      </form>
+      <Link to={'/'}>Ir al Welcome</Link>
+    </div>
   );
 }
 
